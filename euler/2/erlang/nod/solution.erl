1b89dffb6944560ddca270926e2728487a54d8b1
Nodar Chkuaselidze <nodar.chkuaselidze@gmail.com>
2013-10-26 21:32:21 +0400

    change structure

--- euler/2/erlang/nod/solution.erl
+++ euler/2/erlang/nod/solution.erl
@@ -5,15 +5,13 @@ sum(N) when N > 2 ->
   fibonacciSum(1, 2, 2, N).
 
 
+fibonacciSum(_, _, Sum, Max) when Sum > Max ->
+  Sum;
+
 fibonacciSum(F1, F2, Sum, Max)->
   FN = F1 + F2,
 
-  case FN > Max of
-    true ->
-      Sum;
-    _ when FN rem 2 == 0 ->
-      fibonacciSum(F2, FN, Sum + FN, Max);
-    _ ->
-      fibonacciSum(F2, FN, Sum, Max)
+  if
+    FN rem 2 == 0 -> fibonacciSum(F2, FN, Sum + FN, Max);
+    true          -> fibonacciSum(F2, FN, Sum, Max)
   end.
-
